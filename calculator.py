bca5aa47e4c24859f5aca25baf06963a7a354a7f
Alisaberi1 <90723218+Alisaberi1@users.noreply.github.com>
2021-10-01 19:38:31 +0330

    calculator with python

---
+++ calculator.py
@@ -0,0 +1,16 @@
+v=int(input('v:'))
+c=int(input('c:'))
+
+a=input('''for 
+*=* 
+/=/
++=+
+-=-: ''')
+if a=='*':
+	print(v*c)
+if a=='/':
+        print(v/c)
+if a=='+':
+        print(v+c)
+if a=='-':
+        print(v-c)
